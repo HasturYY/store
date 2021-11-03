47b24026406be8d5aed62be82f0b6eeea01c6eb4
HasturYY <92710635+HasturYY@users.noreply.github.com>
2021-11-03 14:16:49 +0800

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,3 +1,5 @@
+from DBUtils import update
+from DBUtils import select
 import random
 print("***************************")
 print("*    中国工商银行          *")
@@ -17,38 +19,81 @@ bank={}
 #定义一个开户银行
 bank_name="埃塞俄比亚银行"
 #定义添加到银行 定义函数元素对应元素  不是名称对名称
+
+
 def bankadd(account,name,password,country,province,steert,door):
-    if len(bank)>=100:
+
+    sql = "select count(*) from users"
+    param = []
+    data = select(sql,param,mode="one")
+    if data[0] >= 100:
         return 3
-    elif name in bank:
+
+    sql1 = "select * from users where username = %s"
+    param1 = [name]
+    data1 = select(sql1,param1)#((),)
+    if len(data1) != 0:
         return 2
-    else:
-        bank[name]={
-            "account":account,
-            "password":password,
-            "country":country,
-            "province":province,
-            "steert":steert,
-            "door":door,
-            "money":0,
-            "bank_name":bank_name
-        }
-        return 1
+
+    sql2 = "insert into users value(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    param2 = [account,name,password,country,province,steert,door,bank_name,0]
+    update(sql2,param2)
+    return 1
+
+    # if len(bank)>=100:
+    #     return 3
+    # elif name in bank:
+    #     return 2
+    # else:
+    #     bank[name]={
+    #         "account":account,
+    #         "password":password,
+    #         "country":country,
+    #         "province":province,
+    #         "steert":steert,
+    #         "door":door,
+    #         "money":0,
+    #         "bank_name":bank_name
+    #     }
+    #     return 1
 
 def access1(name):
-    if name in bank:
-        print("余额：%d"%bank[name]['money'])
-        money=int(input('请输入存储金额：'))+bank[name]['money']
-        bank[name]['money']=money
-        return True
-    else:
-        return False
 
-def take1(name,password,take_money):
-    if name in bank:
-        if password == bank[name]['password']:
-            if take_money <= bank[name]['money']:
-                bank[name]['money']=bank[name]['money']-take_money
+    sql = "select * from users where username = %s"
+    param = [name]
+    date=select(sql,param)
+    print(type(date))
+    if len(date) !=0:
+        money = int(input('请输入存储金额：'))
+        sql = "update users set money = money + %s where username = %s"
+        param = [money,name]
+        update(sql,param)
+        return True
+    else:return False
+
+    # if name in bank:
+    #     print("余额：%d"%bank[name]['money'])
+    #     money=int(input('请输入存储金额：'))+bank[name]['money']
+    #     bank[name]['money']=money
+    #     return True
+    # else:
+    #     return False
+
+def take1(name,password):
+
+    sql="select * from users where username = %s"
+    param = [name]
+    date = select(sql,param)
+    paswd = date[0][2]
+    manei = date[0][8]
+    print(manei)
+    if len(date) != 0:
+        if password ==paswd:
+            money=int(input('请输入取款金额'))
+            if money <=manei:
+                sql="update users set money = money - %s where username = %s"
+                param=[money,name]
+                update(sql,param)
                 return 0
             else:
                 return 3
@@ -56,16 +101,39 @@ def take1(name,password,take_money):
             return 2
     else:
         return 1
+
+
+    # if name in bank:
+    #     if password == bank[name]['password']:
+    #         if take_money <= bank[name]['money']:
+    #             bank[name]['money']=bank[name]['money']-take_money
+    #             return 0
+    #         else:
+    #             return 3
+    #     else:
+    #         return 2
+    # else:
+    #     return 1
 
 def shift1(off_name,on_name,off_passwd,off_money):
-    if off_name in bank and on_name in bank:
-        if off_passwd == bank[off_name]['password']:
-            if off_money<=bank[off_name]['money']:
-                bank[off_name]['money']-=off_money
 
-                bank[on_name]['money']+=off_money
-
-
+    sql = "select * from users where username = %s or username = %s order by username"
+    param = [off_name,on_name]
+    date = select(sql,param)
+    print(date)
+    date1 = date[0][2]
+    date2 = date[0][8]
+    date3 = date[1][8]
+    print(date1,date2,date3)
+    if len(date) >= 2:
+        if off_passwd == date1:
+            if off_money <=date2:
+                sql = "update users set money = money - %s where username = %s"
+                param = [off_money,off_name]
+                update(sql,param)
+                sql1 = "update users set money = money + %s where username = %s"
+                param1 = [off_money, on_name]
+                update(sql1, param1)
                 return 0
             else:
                 return 3
@@ -74,14 +142,48 @@ def shift1(off_name,on_name,off_passwd,off_money):
     else:
         return 1
 
+    # if off_name in bank and on_name in bank:
+    #     if off_passwd == bank[off_name]['password']:
+    #         if off_money<=bank[off_name]['money']:
+    #             bank[off_name]['money']-=off_money
+    #
+    #             bank[on_name]['money']+=off_money
+    #
+    #
+    #             return 0
+    #         else:
+    #             return 3
+    #     else:
+    #         return 2
+    # else:
+    #     return 1
+
 def seek1(name,passwd):
-    if name in bank:
-        if passwd == bank[name]['password']:
+
+    sql = "select * from users where username = %s"
+    param = [name]
+    data = select(sql, param)
+    if len(data) != 0:
+        sql1 = "select password from users where username =%s "
+        param1 = [name]
+        data1 = select(sql1, param1)
+        print(data1)
+        data2=data1[0][0]
+        print(type(passwd),type(data2))
+        if passwd == data2:
             return 0
         else:
             return 2
     else:
         return 1
+
+    # if name in bank:
+    #     if passwd == bank[name]['password']:
+    #         return 0
+    #     else:
+    #         return 2
+    # else:
+    #     return 1
 
 #定义用户入参
 def useradd():
@@ -100,49 +202,48 @@ def useradd():
         print("用户已存在")
     elif num==1:
         print("恭喜你开户成功，一下是您的相信信息")
-        info = '''
-                   ------------个人信息------------
-                   用户名:%s
-                   账号：%s
-                   密码：*****
-                   国籍：%s
-                   省份：%s
-                   街道：%s
-                   门牌号：%s
-                   余额：%s
-                   开户行名称：%s
-               '''
-        # 每个元素都可传入%
-        print(info % (name, account, country, province, steert, door, bank[name]["money"], bank_name))
+        # info = '''
+        #            ------------个人信息------------
+        #            用户名:%s
+        #            账号：%s
+        #            密码：*****
+        #            国籍：%s
+        #            省份：%s
+        #            街道：%s
+        #            门牌号：%s
+        #            余额：%s
+        #            开户行名称：%s
+        #        '''
+        # # 每个元素都可传入%
+        # print(info % (name, account, country, province, steert, door, bank[name]["money"], bank_name))
 
 def access():
     name=input('请输入用户名：')
     num1=access1(name)
     if num1==True:
         print('存储成功！以下是您的详细信息。')
-        info='''
-        ------------个人信息------------
-                   用户名:%s
-                   账号：%s
-                   密码：*****
-                   国籍：%s
-                   省份：%s
-                   街道：%s
-                   门牌号：%s
-                   余额：%s
-                   开户行名称：%s
-        '''
-        print(info % ( name,  bank[name]['account'], bank[name]['country'], bank[name]['province'],bank[name]['steert'] , bank[name]['door'], bank[name]["money"], bank_name))
+        # info='''
+        # ------------个人信息------------
+        #            用户名:%s
+        #            账号：%s
+        #            密码：*****
+        #            国籍：%s
+        #            省份：%s
+        #            街道：%s
+        #            门牌号：%s
+        #            余额：%s
+        #            开户行名称：%s
+        # '''
+        # print(info % ( name,  bank[name]['account'], bank[name]['country'], bank[name]['province'],bank[name]['steert'] , bank[name]['door'], bank[name]["money"], bank_name))
     elif num1==False:
         print('没得')
 
 def take():
     name = input('请输入用户名：')
-    password=input('请输入密码：')
-    take_money=int(input('请输入取出金额：'))
-    num2=take1(name,password,take_money)
+    password=int(input('请输入密码：'))
+    num2=take1(name,password)
     if num2==0:
-        print('取款成功！；您的账户余额为：%d'%bank[name]['money'])
+        print('取款成功！；您的账户余额为：')
     elif num2==1:
         print('么得')
     elif num2==2:
@@ -153,11 +254,10 @@ def take():
 def shift():
     off_name=input('请输入转出用户名')
     on_name=input('请输入转入用户名')
-    off_passwd=input('请输入转出用户密码')
+    off_passwd=int(input('请输入转出用户密码'))
     off_money=int(input('请输入转出的金额'))
     num3=shift1(off_name,on_name,off_passwd,off_money)
     if num3==0:
-        print('转账成功，转出用户余额：%d'%bank[off_name]['money'],'转入用户余额：%d'%bank[on_name]['money'])
         print('转账成功')
     elif num3==1:
         print('么得')
@@ -168,9 +268,14 @@ def shift():
 
 def seek():
     name=input('请输入用户名')
-    passwd=input('请输入密码')
+    passwd=int(input('请输入密码'))
     num4=seek1(name,passwd)
     if num4==0:
+        print('ok')
+        sql="select * from users where username = %s"
+        param=[name]
+        date=select(sql,param,mode='one')
+        print(date)
         info = '''
                 ------------个人信息------------
                            用户名:%s
@@ -182,9 +287,9 @@ def seek():
                            门牌号：%s
                            余额：%s
                            开户行名称：%s
+                ******************************
                 '''
-        print(info % (name, bank[name]['account'], bank[name]['country'], bank[name]['province'], bank[name]['steert'],
-                      bank[name]['door'], bank[name]["money"], bank_name))
+        print(info % (date[1],date[0],date[3],date[4],date[5],date[6],date[8],date[7]))
     elif num4==1:
         print('用户名错误')
     elif num4==2:
